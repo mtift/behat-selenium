fbc9fbab1a66f7316195b2dbd6a4acb8cab812ca
Matthew Tift <me@matthewtift.com>
2018-04-04 09:26:05 -0500

    Use the Drupal context for the FeatureContext class

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,22 +1,22 @@
 <?php
 
-use Behat\Behat\Context\Context;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
-{
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct($parameters) {
+    $this->parameters = $parameters;
+  }
 }
